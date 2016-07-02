e7e50bf241303992f01dd2d386ed5ef1de35755f
YangBo <yang.wilby@gmail.com>
2016-07-02 08:44:16 +0200

    clean up

---
+++ src/test/scala/com/menusystems/uno/api/UnoSimulation.scala
@@ -0,0 +1,28 @@
+package com.menusystems.uno.api
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+/**
+  * Created by wilbyang on 01/07/16.
+  */
+class UnoSimulation extends Simulation{
+
+
+  val httpConf = http
+    .baseURL("https://api.justunoit.com/rest/v1")
+      .basicAuth("admin@justunoit.com", "yangbo")
+
+
+  val scn = scenario("Uno Consumer workflow") // A scenario is a chain of requests and pauses
+    .exec(http("login")
+    .get("/me"))
+    .exec(http("access venues").get("/venues"))
+    .exec(http("access one venue(Langolo)").get("/venues/849157c4-73e8-4523-94a2-058925c27345"))
+
+  setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
+
+
+}
